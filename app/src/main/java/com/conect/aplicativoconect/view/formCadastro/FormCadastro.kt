6b9f823ebc73053e62bca6bd992cde8f5fe2383f
luisxxfelipe <102532989+luisxxfelipe@users.noreply.github.com>
2023-02-08 23:05:28 -0300

    Atualização 1.0

---
+++ app/src/main/java/com/conect/aplicativoconect/view/formCadastro/FormCadastro.kt
@@ -0,0 +1,12 @@
+package com.conect.aplicativoconect.view.formCadastro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.conect.aplicativoconect.R
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
