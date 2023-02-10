2d0512d53a0761fe8eeafcc27ee438acb2e8f9df
luisxxfelipe <102532989+luisxxfelipe@users.noreply.github.com>
2023-02-09 23:25:31 -0300

    Atualização 09/02

--- app/src/main/java/com/conect/aplicativoconect/tela_bem_vindo.kt
+++ app/src/main/java/com/conect/aplicativoconect/tela_bem_vindo.kt
@@ -3,13 +3,14 @@ package com.conect.aplicativoconect
 import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.conect.aplicativoconect.databinding.ActivityTelaBemVindoBinding
 
 class tela_bem_vindo : AppCompatActivity() {
-
-
+    private lateinit var binding: ActivityTelaBemVindoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        binding = ActivityTelaBemVindoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
