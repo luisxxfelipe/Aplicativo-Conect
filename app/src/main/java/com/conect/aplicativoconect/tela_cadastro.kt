3ebb5858aafc857b07c4bf9705e84682d9fccb3d
luisxxfelipe <102532989+luisxxfelipe@users.noreply.github.com>
2023-02-10 22:00:09 -0300

    Atualização oficial 10/02 - Noite

--- app/src/main/java/com/conect/aplicativoconect/tela_cadastro.kt
+++ app/src/main/java/com/conect/aplicativoconect/tela_cadastro.kt
@@ -1,5 +1,6 @@
 package com.conect.aplicativoconect
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.conect.aplicativoconect.databinding.ActivityTelaCadastroBinding
@@ -13,12 +14,19 @@ class tela_cadastro : AppCompatActivity() {
         binding = ActivityTelaCadastroBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.botaoCadastrar.setOnClickListener{view->
+        binding.botaoCadastrar.setOnClickListener { view ->
             val email = binding.cadastroEmail.text.toString()
             val senha = binding.cadastroSenha.text.toString()
 
-            if(email.isEmpty() || senha.isEmpty()){
-                val snackbar: Snackbar.make(it)
+            if (email.isEmpty() || senha.isEmpty()) {
+                val snackbar = Snackbar.make(
+                    view,
+                    "Preencha todos os campos corretamente!",
+                    Snackbar.LENGTH_SHORT
+                )
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+            } else {
             }
         }
     }
--- app/src/main/java/com/conect/aplicativoconect/view/telaLogin/telaLogin.kt
+++ app/src/main/java/com/conect/aplicativoconect/view/telaLogin/telaLogin.kt
@@ -1,16 +1,28 @@
 package com.conect.aplicativoconect.view.telaLogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.conect.aplicativoconect.R
+import com.conect.aplicativoconect.databinding.ActivityTelaLoginBinding
+import com.conect.aplicativoconect.tela_cadastro
 
 class telaLogin : AppCompatActivity() {
 
+    private  lateinit var binding: ActivityTelaLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
-        setContentView(R.layout.activity_tela_login)
+
+        binding = ActivityTelaLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textTelaCadastro.setOnClickListener{
+            val intent = Intent(this,tela_cadastro::class.java)
+            startActivity(intent)
+        }
     }
 }
